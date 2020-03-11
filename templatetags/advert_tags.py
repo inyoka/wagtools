ba69a09448dd0a9c33111de2efd73d7521f940fc
Simon <simonmarkholland@gmail.com>
2020-03-11 14:17:34 +0000

    Add google and facebook advert tags

---
+++ templatetags/advert_tags.py
@@ -0,0 +1,29 @@
+from django import template
+from wagtools.social import Google
+from wagtools.social import Facebook
+
+register = template.Library()
+
+# Google snippets
+@register.inclusion_tag('tags/google.html', takes_context=True)
+def google_site_tag(context):
+    return {
+        'google_tags': Google.objects.all(),
+        'request': context['request'],
+    }
+
+@register.simple_tag
+def google_site_id():
+    return Google.objects.first().site_tag
+
+
+@register.inclusion_tag('tags/facebook.html', takes_context=True)
+def facebook_site_tag(context):
+    return {
+        'facebook_tags': Facebook.objects.all(),
+        'request': context['request'],
+    }
+
+@register.simple_tag
+def facebook_site_id():
+    return Facebook.objects.first().site_tag
