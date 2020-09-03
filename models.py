d911145b5de3c5466b540ae1b8643df3a12c2631
Simon <simonmarkholland@gmail.com>
2020-09-03 10:36:08 +0100

    Ese section page template not home so buttons can be added.

--- models.py
+++ models.py
@@ -116,7 +116,7 @@ class SectionPage(Page, Seo):
     parent_page_types = ['wagtools.SectionIndexPage']
 
     def get_template(self, request):
-        return 'home/home_page.html'
+        return 'sections/section_page.html'
 
     def get_context(self, request):
         context = super(SectionPage, self).get_context(request)
